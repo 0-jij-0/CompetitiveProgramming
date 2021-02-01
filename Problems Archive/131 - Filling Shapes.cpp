0f204a1d0c279ddc7955e32db31ee8bc10fe684b
0-jij-0 <jihadghanna@gmail.com>
2021-02-01 11:14:47 +0200

    Revert "Uploaded Part of Problems Archive (2)"
    This reverts commit 74967bc0f567ac89a8d5ac41b32bb50c1ddb66bb.

--- Problems Archive/131 - Filling Shapes.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-	int n; cin >> n;
-	if (n % 2 == 1) { cout << 0 << endl; }
-	else { cout << (int)floor(pow(2.0, (n + 0.0) / 2.0)) << endl; }
-	cin.ignore(2); return 0;
-}
