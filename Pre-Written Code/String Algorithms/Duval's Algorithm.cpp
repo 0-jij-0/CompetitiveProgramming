bb88e20e1b596d763bd6e76fd7ba64794a8e49aa
0-jij-0 <jihadghanna@gmail.com>
2021-05-30 11:23:19 +0300

    Added Duval's Algorithm for Minimum Lexicographic Rotation

---
+++ Pre-Written Code/String Algorithms/Duval's Algorithm.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+vector<string> duval(string s) {
+    int n = (int)s.size(); vector<string> fact;
+    int i = 0; while (i < n) {
+        int j = i + 1, k = i;
+        while (j < n && s[k] <= s[j]) { s[k] < s[j] ? k = i : k++; j++; }
+        while (i <= k) { fact.push_back(s.substr(i, j - k)); i += j - k; }
+    }
+    return move(fact);
+}
+
+string minCyclicShift(string s) {
+    s += s; int n = (int) s.size(), start = 0;
+    int i = 0; while (i < n / 2) {
+        start = i; int j = i + 1, k = i;
+        while (j < n && s[k] <= s[j]) { s[k] < s[j] ? k = i : k++; j++; }
+        while (i <= k) i += j - k;
+    }
+    return move(s.substr(start, n >> 1));
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    cin.ignore(2); return 0;
+}
