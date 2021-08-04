51921b1724b2441592d5cecb8cbacb94b0f6af94
0-jij-0 <jihadghanna@gmail.com>
2021-08-04 12:12:59 +0300

    Updated RMQ Pre-Written Code

--- Pre-Written Code/Data Structures/RMQ (nlogn).cpp
+++ Pre-Written Code/Data Structures/RMQ (nlogn).cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <numeric>
 using namespace std;
+typedef long long ll;
 
 struct RMQ {
-	vector<vector<int>> rmq;
+	vector<vector<ll>> rmq;
 	vector<int> logb2; RMQ() {}
-	RMQ(vector<int> &v) { //O(nlogn) time and space
+
+	ll combine(ll a, ll b) { return min(a, b); }
+
+	RMQ(vector<int>& v) { //O(nlogn) time and space
 		int n = (int)v.size(); logb2.resize(n + 1, 0);
-		rmq.resize(1, vector<int>(n));
+		rmq.resize(1, vector<ll>(n));
 		for (int i = 0; i < n; i++) { rmq[0][i] = v[i]; }
 
 		for (int i = 0; (1 << i) <= n; i++) { logb2[1 << i] = i; } //Precomputing Logs
@@ -19,14 +22,14 @@ struct RMQ {
 		for (int t = 1; t < (int)rmq.size(); t++) {
 			rmq[t].resize(n - (1 << t) + 1);
 			for (int i = 0; i <= n - (1 << t); i++)
-				rmq[t][i] = min(rmq[t - 1][i], rmq[t - 1][i + (1 << (t - 1))]);
+				rmq[t][i] = combine(rmq[t - 1][i], rmq[t - 1][i + (1 << (t - 1))]);
 		}
 	}
 
-	int query(int i, int j) { //O(1)
+	ll query(int i, int j) { //O(1)
 		if (i > j) { swap(i, j); }
 		int k = logb2[j - i + 1];
-		return min(rmq[k][i], rmq[k][j - (1 << k) + 1]);
+		return combine(rmq[k][i], rmq[k][j - (1 << k) + 1]);
 	}
 };
 
