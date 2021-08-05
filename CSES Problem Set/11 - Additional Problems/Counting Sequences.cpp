c34a880dc55c13f1ee09cff01b2b175a17cf668a
0-jij-0 <jihadghanna@gmail.com>
2021-08-05 11:43:15 +0300

    Solved CSES Additional Problems

---
+++ CSES Problem Set/11 - Additional Problems/Counting Sequences.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+typedef long long ll;
+const ll mod = 1000 * 1000 * 1000 + 7;
+
+vector<ll> fact(1000001, 1);
+
+ll power(ll x, ll n) {
+	ll res = 1;
+	for (; n; n >>= 1, x = (x * x) % mod)
+		if (n & 1) res = (res * x) % mod;
+	return res;
+}
+
+ll nCk(int n, int k) {
+	ll denum = (fact[k] * fact[n - k]) % mod;
+	return (fact[n] * power(denum, mod - 2)) % mod;
+}
+
+int main() {
+	for (int i = 1; i <= 1000000; i++)
+		fact[i] = (i * fact[i - 1]) % mod;
+
+	int n, k; cin >> n >> k; 
+	bool sub = false; ll res = 0;
+	for (int i = 0; i < k; i++, sub ^= 1) {
+		ll cur = (nCk(k, i) * power(k - i, n)) % mod;
+		res = (sub ? res - cur + mod : res + cur) % mod;
+	}
+	cout << res << '\n';
+	cin.ignore(2); return 0;
+}
---
+++ CSES Problem Set/11 - Additional Problems/Multiplication Table.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+typedef long long ll;
+
+ll n;
+
+bool check(ll x) {
+	ll less = 0; for (int i = 1; i <= n; i++)
+		less += min((x - 1) / i, n);
+
+	return less <= ((n * n) >> 1);
+}
+
+ll find_middle() {
+	ll l = 1, r = n * n;
+	while (l != r) {
+		ll mid = (l + r + 1) >> 1;
+		check(mid) ? l = mid : r = mid - 1;
+	}
+	return l;
+}
+
+int main() {
+	cin >> n; cout << find_middle() << '\n';
+	cin.ignore(2); return 0;
+}
