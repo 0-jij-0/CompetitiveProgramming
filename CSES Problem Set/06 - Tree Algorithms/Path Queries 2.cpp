42039db7886808f8acf121f31cc510a298e3ceb7
0-jij-0 <jihadghanna@gmail.com>
2021-10-16 20:36:46 +0300

    Solve a CSES Tree Algorithms Problem

--- CSES Problem Set/06 - Tree Algorithms/Path Queries 2.cpp
+++ CSES Problem Set/06 - Tree Algorithms/Path Queries 2.cpp
@@ -2,7 +2,6 @@
 #include <vector>
 #include <algorithm>
 #include <numeric>
-#include <array>
 using namespace std;
 typedef long long ll;
 
@@ -15,25 +14,14 @@ struct StVal {
 	operator ll() const { return v; }
 };
 
-struct StUpdate {
-	ll u = 0; StUpdate() {}
-	StUpdate(const ll u) : u(u) {}
-	StUpdate(const StUpdate& u1, const StUpdate& u2) { u = u1 + u2; }
-	operator ll() const { return u; }
-	void apply(StVal& v, const int lo, const int hi) { v.v += u; }
-};
-
 struct SegTree {
 	vector<StVal> st;
-	vector<StUpdate> lazy;
 	vector<int> leaves;
 	int n = 0; SegTree() {}
 
-	SegTree(const int n, vector<int> &arr) : n(n), leaves(n) {
-		init(1, 0, n - 1, arr); lazy.resize(st.size());
-	}
+	SegTree(const int n, vector<int>& arr) : n(n), leaves(n) { init(1, 0, n - 1, arr); }
 
-	void init(const int si, const int lo, const int hi, vector<int> &arr) {
+	void init(const int si, const int lo, const int hi, vector<int>& arr) {
 		if (lo == hi) {
 			if (si >= (int)st.size()) st.resize(si + 1);
 			st[si] = StVal(value[arr[lo]]); leaves[lo] = si;
@@ -46,18 +34,8 @@ struct SegTree {
 		}
 	}
 
-	void updateLazy(const int si, const int lo, const int hi) {
-		lazy[si].apply(st[si], lo, hi);
-		if (lo != hi) {
-			lazy[si << 1] = StUpdate(lazy[si << 1], lazy[si]);
-			lazy[si << 1 | 1] = StUpdate(lazy[si << 1 | 1], lazy[si]);
-		}
-		lazy[si] = StUpdate();
-	}
-
 	StVal query(const int l, const int r) { return (l <= r && l < n&& r >= 0) ? query(l, r, 1, 0, n - 1) : StVal(); }
 	StVal query(const int l, const int r, const int si, const int lo, const int hi) {
-		updateLazy(si, lo, hi);
 		if (l <= lo && hi <= r) return st[si];
 
 		const int mid = (lo + hi) >> 1;
@@ -66,47 +44,22 @@ struct SegTree {
 		return StVal(query(l, r, si << 1, lo, mid), query(l, r, si << 1 | 1, mid + 1, hi));
 	}
 
-	void update(const int l, const int r, const StUpdate u) { if (l <= r) update(l, r, u, 1, 0, n - 1); }
-	void update(const int l, const int r, const StUpdate& u, const int si, const int lo, const int hi) {
-		if (l <= lo && hi <= r) {
-			lazy[si] = StUpdate(lazy[si], u);
-			updateLazy(si, lo, hi);
-		}
-		else {
-			updateLazy(si, lo, hi);
-			if (hi < l || r < lo) return;
-			const int mid = (lo + hi) >> 1;
-			update(l, r, u, si << 1, lo, mid);
-			update(l, r, u, si << 1 | 1, mid + 1, hi);
-			st[si] = StVal(st[si << 1], st[si << 1 | 1]);
-		}
-	}
-
-	void update(const int i, StUpdate U) {
-		int si = leaves[i]; lazy[si] = StUpdate(lazy[si], U);
-		updateLazy(si, i, i); for (si >>= 1; si; si >>= 1)
+	void update(const int i, int x) {
+		int si = leaves[i]; st[si] = x;
+		for (si >>= 1; si; si >>= 1)
 			st[si] = StVal(st[si << 1], st[si << 1 | 1]);
 	}
 };
-
-struct edge {
-	int u, v; edge() {}
-	edge(int _u, int _v) :
-		u(_u), v(_v) {}
-};
-
-struct node { vector<edge> edges; };
 
 struct HLDTree {
-	vector<node> nodes; int n; //Change based on N
-	vector<int> par, depth, subtreeSize; //Required Information for HLD
+	vector<vector<int>> nodes; int n;
+	vector<int> par, depth, subtreeSize; //For LCA and Heavy Edges
 	vector<int> segtreeIDX, pathIDX, nextPath;
 	vector<vector<int>> heavyPaths;
 	vector<SegTree> st;
 
-	HLDTree(int _n) : n(_n), nodes(_n), depth(_n, 0), par(_n),
-		subtreeSize(_n, 1),	segtreeIDX(_n), pathIDX(_n) {
-		
+	HLDTree(int _n) : n(_n), nodes(_n), depth(_n, 0), par(n, -1),
+		subtreeSize(_n, 1), segtreeIDX(_n), pathIDX(_n) {
 		value.resize(n); for (auto& x : value) cin >> x;
 		for (int i = 1; i < _n; i++) {
 			int u, v; cin >> u >> v;
@@ -117,15 +70,15 @@ struct HLDTree {
 	}
 
 	void add_edge(int u, int v) {
-		nodes[u].edges.emplace_back(u, v);
-		nodes[v].edges.emplace_back(v, u);
+		nodes[u].emplace_back(v);
+		nodes[v].emplace_back(u);
 	}
 
 	//Finds parents, depths & substreeSizes
-	int initDFS(int cur, int p = -1) { 
-		for (auto& e : nodes[cur].edges) if (e.v != p) {
-			par[e.v] = cur; depth[e.v] = 1 + depth[cur];
-			subtreeSize[cur] += initDFS(e.v, cur);
+	int initDFS(int cur, int p = -1) {
+		for (auto& e : nodes[cur]) if (e != p) {
+			par[e] = cur; depth[e] = 1 + depth[cur];
+			subtreeSize[cur] += initDFS(e, cur);
 		}
 		return subtreeSize[cur];
 	}
@@ -142,13 +95,11 @@ struct HLDTree {
 			int subSize = subtreeSize[cur];
 			int heavy = (subSize + (subSize & 1)) >> 1;
 
-			for (auto& e : nodes[cur].edges) {
-				if (e.v == par[cur]) { continue; }
-
+			for (auto& e : nodes[cur]) if(e != par[cur]) {
 				//End of previous heavy path
-				if (subtreeSize[e.v] < heavy) { addHeavyPath(e.v, cur); continue; }
+				if (subtreeSize[e] < heavy) { addHeavyPath(e, cur); continue; }
 
-				newPaths = false; pathIDX[cur] = pathIDX[e.v];
+				newPaths = false; pathIDX[cur] = pathIDX[e];
 				heavyPaths[pathIDX[cur]].push_back(cur);
 			}
 			if (newPaths) { //Node doesn't have a down heavy edge
@@ -159,7 +110,7 @@ struct HLDTree {
 		}
 
 		addHeavyPath(0); for (int i = 0; i < (int)nextPath.size(); i++)
-			if(nextPath[i] != -1) nextPath[i] = pathIDX[nextPath[i]];
+			if (nextPath[i] != -1) nextPath[i] = pathIDX[nextPath[i]];
 	}
 
 	//Adds a Heavy Path, Initializes Corresponding Segment Trees
@@ -173,8 +124,9 @@ struct HLDTree {
 		nextPath[idx] = p;
 	}
 
-	StVal query(int a, int b) { //O(log^2(n))
+	StVal query(int a, int b) {
 		int A = pathIDX[a], B = pathIDX[b];
+
 		StVal res = StVal(); while (A != B) {
 			if (depth[heavyPaths[A][0]] < depth[heavyPaths[B][0]]) { swap(A, B); swap(a, b); }
 			res = StVal(res, st[A].query(0, segtreeIDX[a]));
@@ -185,25 +137,27 @@ struct HLDTree {
 		return StVal(res, st[A].query(L, R));
 	}
 
-	void pointUpdate(int a, StUpdate U) {
-		st[pathIDX[a]].update(segtreeIDX[a], U);
-	}
-	void rangeUpdate(int a, int b, StUpdate U) { //O(log^2(n))
-		int A = pathIDX[a], B = pathIDX[b];
-		while (A != B) {
-			if (depth[heavyPaths[A][0]] < depth[heavyPaths[B][0]]) { swap(A, B); swap(a, b); }
-			st[A].update(0, segtreeIDX[a], U);
-			a = par[heavyPaths[A][0]]; A = nextPath[A];
-		}
-		int L = min(segtreeIDX[a], segtreeIDX[b]);
-		int R = max(segtreeIDX[a], segtreeIDX[b]);
-		st[A].update(L, R, U);
+	void pointUpdate(int a, int x) {
+		st[pathIDX[a]].update(segtreeIDX[a], x);
 	}
 };
 
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0), cout.tie(0);
-	
+
+	int n, q; cin >> n >> q;
+	HLDTree hld(n); while (q--) {
+		int t; cin >> t;
+		if (t == 1) {
+			int s, x; cin >> s >> x; s--;
+			hld.pointUpdate(s, x);
+		}
+		else {
+			int a, b; cin >> a >> b; a--; b--;
+			cout << hld.query(a, b) << '\n';
+		}
+	}
+
 	cin.ignore(2); return 0;
 }
