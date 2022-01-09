fefb81f7f064099e84709b8ca3aabb163bad5cc8
0-jij-0 <jihadghanna@gmail.com>
2022-01-09 20:12:33 +0200

    Optimized one CSES Math Problem's Solution

--- Problems Archive/1000 - 1999/1125 - Common Divisors.cpp
+++ Problems Archive/1000 - 1999/1125 - Common Divisors.cpp
@@ -1,50 +1,21 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
-typedef long long ll;
-const int SVSZ = 1000009;
 
-int spf[SVSZ]; //Smallest Prime Factor
-vector<int> primes; int res = 0;
-vector<bool> p(SVSZ, false);
-
-void buildSieve() {
-	vector<int> primes; spf[0] = spf[1] = 1;
-	for (int i = 2; i < SVSZ; i++) {
-		if (spf[i] == 0) { primes.push_back(i);	spf[i] = i; }
-		for (int j = 0; j < int(primes.size()) && primes[j] <= spf[i] && i * primes[j] < SVSZ; j++)
-			spf[i * primes[j]] = primes[j];
-	}
-}
-
-void getDivisorsHelper(int i, int curr, vector<pair<int, int>> &v) {
-	if (i == (int)v.size()) { 
-		if (p[curr]) { res = max(res, curr); }
-		p[curr] = true; return;
-	}
-	getDivisorsHelper(i + 1, curr, v);
-	for (int j = 0; j < v[i].second; j++) {
-		curr *= v[i].first;
-		getDivisorsHelper(i + 1, curr, v);
-	}
-}
-
-void getDivisors(int x) {
-	vector<pair<int, int>> v;
-	while (x > 1) {
-		int d = spf[x], cnt = 0;
-		while (x % d == 0) { x /= d; cnt++; }
-		v.push_back({ d, cnt });
-	}
-	getDivisorsHelper(0, 1, v);
-}
+vector<int> v(1000001, 0);
+const int N = 1000000;
 
 int main() {
 	ios::sync_with_stdio(0);
-	cin.tie(0), cout.tie(0);
+	cin.tie(0); cout.tie(0);
 
-	int n; cin >> n; buildSieve();
-	while (n--) { int x; cin >> x; getDivisors(x); }
-	cout << res << endl; cin.ignore(2); return 0;
+	int n; cin >> n; while (n--) {
+		int x; cin >> x; v[x]++;
+	}
+
+	for (int i = N, res = 0; i > 0; i--, res = 0) {
+		for (int j = i; j <= N; j += i)	res += v[j];
+		if (res > 1) { cout << i << '\n'; break; }
+	}
+	cin.ignore(2); return 0;
 }
