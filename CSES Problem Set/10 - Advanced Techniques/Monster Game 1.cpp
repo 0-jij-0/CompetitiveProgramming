cfa043806154eaa1c8a97d4151db4d136ad31159
0-jij-0 <jihadghanna@gmail.com>
2022-07-22 15:20:07 +0300

    Solved CSES Advanced Techniques Problems

---
+++ CSES Problem Set/10 - Advanced Techniques/Monster Game 1.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+using namespace std;
+typedef long long ll;
+
+struct Point {
+	ll x = 0, y = 0;
+	Point(ll _x, ll _y) : x(_x), y(_y) {}
+	Point operator - (const Point& rhs) const { return Point(x - rhs.x, y - rhs.y); }
+};
+
+ll dotProduct(Point A, Point B) { return A.x * B.x + A.y * B.y; }
+ll crossProduct(Point A, Point B) { return A.x * B.y - A.y * B.x; }
+Point normal(Point P) { return Point(-P.y, P.x); }
+
+deque<Point> convexHull, normalVectors;
+
+void addLineCHT(ll k, ll b) { // Adding the line kx + b
+	Point P(k, b); while (!normalVectors.empty()) {
+		if (dotProduct(normalVectors.back(), P - convexHull.back()) > 0) break;
+		normalVectors.pop_back(); convexHull.pop_back();
+	}
+
+	if (!convexHull.empty()) normalVectors.emplace_back(normal(convexHull.back() - P));
+	convexHull.push_back(P);
+}
+
+vector<ll> dp, s, f;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int n, x; cin >> n >> x;
+	s.resize(n); for (auto& x : s) cin >> x;
+	f.resize(n); for (auto& x : f) cin >> x;
+	dp.resize(n, 0); addLineCHT(x, 0);
+
+	for (int i = 0; i < n; i++) {
+		Point P(s[i], 1); while (!normalVectors.empty()) {
+			if (crossProduct(normalVectors.front(), P) >= 0) break;
+			normalVectors.pop_front(); convexHull.pop_front();
+		}
+		dp[i] = dotProduct(P, convexHull.front());
+		addLineCHT(f[i], dp[i]);
+	}
+
+	cout << dp[n - 1] << '\n';
+}
---
+++ Problems Archive/2000 - 2999/2115 - Monster Game 2.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+typedef long long ll;
+const ll INF = 1ll << 60;
+
+template<class Type> struct Line {
+	Type a = 1, b = 0;
+	Line(Type _a, Type _b) : a(_a), b(_b) {}
+	Type operator()(Type X) { return a * X + b; }
+};
+
+template<class Type> struct LiChaoTree {
+	vector<Line<Type>> lines; int n;
+
+	//Change these to toggle min/max
+	LiChaoTree(int _n) : n(_n), lines((_n + 1) << 2, Line<Type>(0, INF)) {}
+	Type combine(Type a, Type b) { return min(a, b); }
+	bool check(Type a, Type b) { return a < b; }
+
+	void insert(Line<Type> curLine) { insert(curLine, 0, n, 1); }
+	void insert(Line<Type> curLine, int L, int R, int cur) {
+		if (L == R) {
+			if (check(curLine(L), lines[cur](L)))
+				lines[cur] = curLine;
+			return;
+		}
+
+		int M = (L + R) >> 1;
+		bool left = check(curLine(L), lines[cur](L));
+		bool mid = check(curLine(M), lines[cur](M));
+
+		if (mid) { swap(lines[cur], curLine); }
+		if (left != mid) insert(curLine, L, M, cur << 1);
+		else insert(curLine, M + 1, R, cur << 1 | 1);
+	}
+
+	Type query(Type X) { return query(X, 0, n, 1); }
+	Type query(Type X, int L, int R, int cur) {
+		if (L == R) { return lines[L](X); }
+
+		int M = (L + R) >> 1, left = cur << 1, right = cur << 1 | 1;
+		if (X <= M) return combine(lines[cur](X), query(X, L, M, left));
+		else return combine(lines[cur](X), query(X, M + 1, R, right));
+	}
+};
+
+vector<ll> S, F;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
+
+	int n, x; cin >> n >> x; LiChaoTree<ll> lct(1000*1000);
+	S.resize(n); for (auto& x : S) cin >> x;
+	F.resize(n); for (auto& x : F) cin >> x;
+
+	vector<ll> DP(n); lct.insert(Line<ll>(x, 0));
+	for (int i = 0; i < n; i++) {
+		DP[i] = lct.query(S[i]);
+		lct.insert(Line<ll>(F[i], DP[i]));
+	}
+
+	cout << DP.back() << '\n';
+}
